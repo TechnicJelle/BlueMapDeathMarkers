90a6abdfd640d29fa9af2a808653e2066c50a81a
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2023-11-25 13:13:59 +0100

    Unpack wildcard import

--- src/main/java/com/technicjelle/bluemapdeathmarkers/BlueMapDeathMarkers.java
+++ src/main/java/com/technicjelle/bluemapdeathmarkers/BlueMapDeathMarkers.java
@@ -21,8 +21,10 @@ import org.jetbrains.annotations.Nullable;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.*;
-import java.nio.file.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.logging.Level;
